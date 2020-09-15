9e28c3a047467e7da50d3bbabf08b409016c5e44
HYOSEOK LEE <lhseok0703@naver.com>
2020-09-15 19:47:03 +0900

    12번 수정, 곱셈표

--- src/Chapter01/MultiplyTable.java
+++ src/Chapter01/MultiplyTable.java
@@ -11,7 +11,7 @@ public class MultiplyTable {
 		for (int i = 1; i <= 9; i++) {
 			System.out.printf("%2d |", i);
 			for (int j = 1; j <= 9; j++)
-				System.out.printf("%3d", i + j);
+				System.out.printf("%3d", i * j);
 			System.out.println();
 		}
 	}
